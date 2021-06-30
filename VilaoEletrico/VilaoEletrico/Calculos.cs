516e85577f801fd0e6865215c21cb487452535dd
Paulo Mariano <paulomariano@infosiscs.com>
2021-06-30 17:33:36 -0300

    Atualização da estrutura de dados

---
+++ VilaoEletrico/VilaoEletrico/Calculos.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VilaoEletrico
+{
+
+    public interface Ic
+    {
+      double valorTotal { get; set; }
+
+      
+      double sICMS { get; set; }
+
+
+        double sPIS { get; set; }
+
+        double sCOFINS { get; set; }
+
+    }
+    public class Calculos
+    {
+
+
+        public static ObjetoCasa MaiorConsumo(List<ObjetoCasa> ObjetosDaCasa)
+        {
+            ObjetoCasa maiorConsumo = null;
+
+            foreach (ObjetoCasa item in ObjetosDaCasa)
+            {
+                if(maiorConsumo == null 
+                    || (maiorConsumo.ConsumoWatts * maiorConsumo.TempoUsoDia * 30) < (item.ConsumoWatts * item.TempoUsoDia*30))
+                {
+                    maiorConsumo = item;
+                }
+            }
+
+            return maiorConsumo;
+        }
+
+        public static ObjetoCasa MenorConsumo(List<ObjetoCasa> ObjetosDaCasa)
+        {
+            ObjetoCasa menorConsumo = null;
+
+            foreach (ObjetoCasa item in ObjetosDaCasa)
+            {
+                if (menorConsumo == null
+                    || (menorConsumo.ConsumoWatts * menorConsumo.TempoUsoDia * 30) > (item.ConsumoWatts * item.TempoUsoDia * 30))
+                {
+                    menorConsumo = item;
+                }
+            }
+
+            return menorConsumo;
+        }
+
+        public static ObjetoCasa MaiorTempoDeUso(List<ObjetoCasa> ObjetosDaCasa)
+        {
+            ObjetoCasa maiorTempoDeUso = null;
+
+            foreach (ObjetoCasa item in ObjetosDaCasa)
+            {
+                if (maiorTempoDeUso == null
+                    || (item.TempoUsoDia) > (maiorTempoDeUso.TempoUsoDia))
+                {
+                    maiorTempoDeUso = item;
+                }
+            }
+
+            return maiorTempoDeUso;
+        }
+
+        public static ObjetoCasa MenorTempoDeUso(List<ObjetoCasa> ObjetosDaCasa)
+        {
+            ObjetoCasa menorTempoDeUso = null;
+
+            foreach (ObjetoCasa item in ObjetosDaCasa)
+            {
+                if (menorTempoDeUso == null ||
+                    (item.TempoUsoDia) < (menorTempoDeUso.TempoUsoDia))
+                {
+
+
+                    menorTempoDeUso = item;
+                }
+            }
+
+            return menorTempoDeUso;
+        }
+
+     
+     
+    }
+}
--- VilaoEletrico/VilaoEletrico/Program.cs
+++ VilaoEletrico/VilaoEletrico/Program.cs
@@ -4,24 +4,29 @@ using System.Collections.Generic;
 // INCOMPLETO
 namespace VilaoEletrico
 {
-
     class Program
     {
+
         public static void Main(string[] args)
         {
             DadoTarifaEConta dado = new DadoTarifaEConta();
             List<ObjetoCasa> objetosDaCasa = new List<ObjetoCasa>();
 
-            Console.WriteLine("Quantos dados deseja inserir?");
-            int entrada = Convert.ToInt32(Console.ReadLine());
+            List<string> nome = new List<string>();
+            List<string> local = new List<string>();
+            List<double> consumowatts = new List<double>();
+            List<int> tempo = new List<int>();
 
-            string[] nome = new string[entrada];
-            string[] local = new string[entrada];
-            double[] consumowatts = new double[entrada];
-            int[] tempo = new int[entrada];
-            double ValorTotal = 35.20; // CUSTO ILUMINAÇÃO PUBLICA
+            double ValorTotal = 35.20;
+            double somaICMS = 0;
+            double somaPIS = 0;
+            double somaCOFINS = 0;
+
+            // CUSTO ILUMINAÇÃO PUBLICA
             bool cond = true;
             // FALTA SEPARAR AS CLASSES EM CADA ARQUIVO E ADICIONAR UM RETORNO QUANDO A BANDEIRA FOR INVÁLIDA
+
+            bool condicao = true;
 
             Console.WriteLine("Entre com o valor da tarifa:");
             dado.ValorTarifa = Convert.ToDouble(Console.ReadLine());
@@ -50,84 +55,90 @@ namespace VilaoEletrico
 
             Console.WriteLine("Entre com o valor do imposto ICMS em porcentagem");
             dado.ICMS = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("---------------------");
 
-            /////////////////////////////////////////////////////
-            int maiorUso = 0;
-            int menorUso = 0;
-            int maiorConsumo = 0;
-            int menorConsumo = 0;
-
-            for (int i = 0; i < entrada; i++)
+            while (condicao)
             {
+                /////////////////////////////////////////////////////
+                ObjetoCasa objetoCasa = new ObjetoCasa();
 
                 Console.WriteLine("Entre com o nome do aparelho");
-                nome[i] = Console.ReadLine();
+                string vnome = Console.ReadLine();
+                objetoCasa.Nome = vnome;
 
                 Console.WriteLine("Entre com o nome do local");
-                local[i] = Console.ReadLine();
+                string localv = Console.ReadLine();
+                objetoCasa.Local = (localv);
 
                 Console.WriteLine("Entre com o valor do consumo em Watt/h");
-                consumowatts[i] = Convert.ToDouble(Console.ReadLine());
+                double consumov = Convert.ToDouble(Console.ReadLine());
+                objetoCasa.ConsumoWatts = (consumov);
 
                 Console.WriteLine("Entre com o tempo de uso diário do item");
-                tempo[i] = Convert.ToInt32(Console.ReadLine());
+                int tempov = Convert.ToInt32(Console.ReadLine());
+                objetoCasa.TempoUsoDia = (tempov);
+
+                objetosDaCasa.Add(objetoCasa);
 
                 Console.WriteLine("---------------------");
 
-                //organizar tempo maior
-                for (int j = 0; j < entrada; j++)
+                Console.WriteLine("Deseja inserir novos dados? (S,N) ");
+                char fimPrograma = Convert.ToChar(Console.ReadLine());
+
+                if (fimPrograma == 'n' || fimPrograma == 'N')
                 {
-                    if (tempo[j] > tempo[maiorUso])
-                    {
-                        maiorUso = j;
-                    }
-
-                    //organizar tempo menor
-                    if (tempo[j] < tempo[menorUso])
-                    {
-                        menorUso = j;
-                    }
-
-                    // organizar consumo maior
-
-                    if (consumowatts[j] > consumowatts[maiorConsumo])
-                    {
-                        maiorConsumo = j;
-                    }
-
-                    //organizar consumo menor
-
-                    if (consumowatts[j] < consumowatts[menorConsumo])
-                    {
-                        menorConsumo = j;
-                    }
+                    condicao = false;
                 }
+
                 double resultadoParcial;
                 double resultadoPIS;
                 double resultadoCOFINS;
-                double ConsumoMes = (consumowatts[i] / 1000) * tempo[i] * 30;
+                double ConsumoMes = (objetoCasa.ConsumoWatts / 1000) * objetoCasa.TempoUsoDia * 30;
 
                 resultadoParcial = ConsumoMes * dado.ValorTarifa;
                 resultadoPIS = (dado.PIS / 100) * resultadoParcial;            //dividido por 100 para conseguir o valor correto pro cálculo
                 resultadoCOFINS = (dado.COFINS / 100) * resultadoParcial;    //dividido por 100 para conseguir o valor correto pro cálculo
                 ValorTotal += (resultadoPIS + resultadoCOFINS + ConsumoMes) * ((dado.ICMS / 100) + 1); //1.25 = 25% de acréscimo
 
-                objetosDaCasa.Add(new ObjetoCasa() { Nome = nome[i], Local = local[i], ConsumoWatts = consumowatts[i], TempoUsoDia = tempo[i] });
+                somaPIS += resultadoPIS;
+                somaICMS += (dado.ICMS / 100) * resultadoParcial;
+                somaCOFINS = resultadoCOFINS;
+
             }
+
+            ObjetoCasa objMaiorC = Calculos.MaiorConsumo(objetosDaCasa);
+            ObjetoCasa objMenorC = Calculos.MenorConsumo(objetosDaCasa);
+            ObjetoCasa objMaiorT = Calculos.MaiorTempoDeUso(objetosDaCasa);
+            ObjetoCasa objMenorT = Calculos.MenorTempoDeUso(objetosDaCasa);
+
+
             foreach (var item in objetosDaCasa)
             {
+                Console.WriteLine("---------------------------------------------");
                 Console.WriteLine("Nome do item: {0} ", item.Nome);
                 Console.WriteLine("Nome do local : {0}", item.Local);
                 Console.WriteLine("Consumo do item em Kw/h: {0}", item.ConsumoWatts);
                 Console.WriteLine("Tempo de uso no dia : {0}", item.TempoUsoDia);
                 Console.WriteLine("Bandeira: {0}", dado.Bandeira);
-                Console.WriteLine("---------------------------------------------");
+                // Console.WriteLine("Valor de impostos :{0}", resultadoPIS);
+
             }
-            Console.WriteLine("Valor total a se pagar {0}:", ValorTotal);
-            Console.WriteLine("O local com MAIOR tempo de uso {0}", local[maiorUso]);
-            Console.WriteLine("O local com MENOR gasto de uso {0}", local[menorConsumo]);
-            Console.WriteLine("O local com MENOR tempo foi {0}", local[menorUso]);
-            Console.WriteLine("O local com MAIOR gasto foi {0}", local[maiorConsumo]);
+
+            Console.WriteLine("---------------------------------------------");
+
+            Console.WriteLine("Valor total a se pagar: {0}", Math.Round(ValorTotal, 2));
+
+            Console.WriteLine("Valor somado do PIS: {0}", Math.Round(somaPIS, 2));
+            Console.WriteLine("Valor somado do COFINS: {0}", Math.Round(somaCOFINS, 2));
+            Console.WriteLine("Valor somado do ICMS: {0}", Math.Round(somaICMS, 2));
+
+            Console.WriteLine("---------------------------------------------");
+
+            Console.WriteLine("O local com MAIOR tempo de uso {0}", objMaiorT.Nome);
+            Console.WriteLine("O local com MENOR gasto de uso {0}", objMenorC.Nome);
+
+            Console.WriteLine("O local com MENOR tempo foi {0}", objMenorT.Nome);
+            Console.WriteLine("O local com MAIOR gasto foi {0}", objMaiorC.Nome);
         }
     }
 }
